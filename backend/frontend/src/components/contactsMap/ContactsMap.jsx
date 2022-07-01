df276d9961bdc1ed6a0de822492337174a77f44b
Okonesh02 <freegod2805@gmail.com>
2022-07-01 14:38:52 +0900

    Updated contacts map

--- backend/frontend/src/components/contactsMap/ContactsMap.jsx
+++ backend/frontend/src/components/contactsMap/ContactsMap.jsx
@@ -8,17 +8,18 @@ function ContactsMap(){
     return(
         <div className="contactsMap__block">
             <div className="contactsMap__container">
-                <YMaps>
+                {/* <YMaps>
                     <Map state={mapState} className='contactsMap__map'>
                         <Placemark geometry={mapPlacemarkGeometry}/>
                     </Map>
-                </YMaps>
+                </YMaps> */}
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Afa51e6c28df90ba42cb5e98612768fbc28871de0ed99e6319c18e8175ba34dd3&amp;source=constructor" className='contactsMap__map' frameborder="0"></iframe>
                 <div className="contactsMap__content">
                     <div className="contactsMap__content-title">
                         MONDINO tracker  - мобильные технологии на страже вашего здоровья.
                     </div>
                     <div className="contactsMap__content-address">
-                        Россия, Республика Саха (Якутия), г.Якутск, ул. Ленина 1
+                        Россия, Республика Саха (Якутия), г.Якутск, Проспект Ленина 1
                     </div>
                     <div className="contactsMap__content-name">
                         IT Park Yakutsk
