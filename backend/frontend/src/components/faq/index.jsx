e2f1623999457798965dc9d465ede5e040e325de
Okonesh02 <freegod2805@gmail.com>
2022-07-08 17:35:31 +0900

    Updated faq

--- backend/frontend/src/components/faq/index.jsx
+++ backend/frontend/src/components/faq/index.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
 
 function Faq() {
-    const [texts, setTexts] = useState([
+    const texts = [
         {
-            id: 0,
             buttonText: "Почему существует этот проект?",
             texts: [
                 "DDI Pocket Medic Online Сервис вырос из конкретной цели: сделать удобную единую платформу, где врачи и обычные пользователи проверяли взаимодействие выписанных лекарственных препаратов при одновременном приеме.",
@@ -13,39 +12,33 @@ function Faq() {
                 "Важно понимать, что сервис не заменяет врача, она разработана для того, чтобы понизить риск назначения лекарств с опасным взаимодействием.",
                 "Мы рады, что можем предоставить продукт которая служит для повышения качества жизни обычных граждан.",
             ],
-            close: true,
         },
         {
-            id: 1,
             buttonText: "Что означает «DDI»?",
             texts: [
                 "Интернатура дерматовенерология, хирургия. Ординатура на кафедре пластической хирургии ПМГМУ им. И.М. Сеченова",
             ],
-            close: true,
         },
-    ])
-    const handleFaqText = (id, newCloseValue) => {
-        let newTexts = [...texts]
-        newTexts[id].close = newCloseValue
-        setTexts(newTexts)
-    }
+    ]
+    // const handleFaqText = (id) => {
+    //     texts[id].close ? texts[id].close = false : texts[id].close = true
+    //     console.log(texts)
+    // }
     return(
         <Box>
             {texts.map((item, index) => (
-                <Box key={index}>
-                    <Typography onClick={handleFaqText(item.id, item.close ? false : true)} sx={{ cursor: "pointer" }}>{item.buttonText}</Typography>
-                    {item.close ? 
-                        <Box sx={{ display: "none" }}></Box>
-                        :
-                        <Box>
-                            {item.texts.map((itemText, index) => (
-                                <Typography key={index}>
-                                    {itemText}
-                                </Typography>
-                            ))}
-                        </Box>
-                    }
-                </Box>
+                <Accordion key={index}>
+                    <AccordionSummary>
+                        <Typography sx={{ display: "inline-block", cursor: "pointer" }}>{item.buttonText}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        {item.texts.map((itemText, index) => (
+                            <Typography key={index}>
+                                {itemText}
+                            </Typography>
+                        ))}
+                    </AccordionDetails>
+                </Accordion>
             ))}
         </Box>
     )
