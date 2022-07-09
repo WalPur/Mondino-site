cd5c97058f34520cdf0b22374ceaaa3280648aa9
Okonesh02 <freegod2805@gmail.com>
2022-07-09 20:51:33 +0900

    Added containers in footer, header, fixed ease, health images

--- backend/frontend/src/components/header/Header.jsx
+++ backend/frontend/src/components/header/Header.jsx
@@ -9,23 +9,25 @@ function Header() {
 
     return (
         <header className="container-fluid" id="header">
-            <NavLink className={({ isActive }) => (isActive ? 'active nav-link logo-img' : 'inactive nav-link logo-img')}  to="/">
-                <img src={Images.logo}/>
-            </NavLink>
-            <Navbar expand="lg" className="navbar navbar-expand-lg navbar-light">
-                <Container>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto navbar-nav">
-                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/">Главная</NavLink>
-                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/team">Команда</NavLink>
-                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/contacts">Контакты</NavLink>
-                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')} to="/interactions">DDI</NavLink>
-                            <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')} to="/application">Заявка для партнерства</NavLink>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <div className="header__container container">
+                <NavLink className={({ isActive }) => (isActive ? 'active nav-link logo-img' : 'inactive nav-link logo-img')}  to="/">
+                    <img src={Images.logo}/>
+                </NavLink>
+                <Navbar expand="lg" className="navbar navbar-expand-lg navbar-light">
+                    <Container>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto navbar-nav">
+                                <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/">Главная</NavLink>
+                                <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/team">Команда</NavLink>
+                                <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')}  to="/contacts">Контакты</NavLink>
+                                <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')} to="/interactions">DDI</NavLink>
+                                <NavLink className={({ isActive }) => (isActive ? 'active nav-link' : 'inactive nav-link')} to="/application">Заявка для партнерства</NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
         </header>
     );
 }
