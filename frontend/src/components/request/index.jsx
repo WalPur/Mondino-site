15cf2956e1a634ebaf11fbe418925068805b07e8
NurgunIvanov <105852325+NurgunIvanov@users.noreply.github.com>
2022-07-15 17:46:15 +0900

    request adaptive

--- frontend/src/components/request/index.jsx
+++ frontend/src/components/request/index.jsx
@@ -4,20 +4,30 @@ import { useNavigate, useParams } from "react-router-dom";
 import { Box, Typography, Button } from "@mui/material";
 import { styled } from '@mui/system';
 
+const CustomBox = styled(Box)(({ theme }) => ({
+    height: "80vh",
+    display: "flex",
+    alignItems: "center",
+    // padding: "100px 0 219px",
+    [theme.breakpoints.down("sm")]:{
+        padding: "30px 0",
+    }
+}));
 const MyTextTitle = styled(Typography)(({ theme }) => ({
-    fontWeight: 500,
+    fontWeight: 600,
     fontSize: 41,
-    lineHeight: "63px",
+    // lineHeight: "63px",
     marginBottom: 23,
     // marginTop: 20,
     color: "#343434",
     [theme.breakpoints.down("sm")]:{
-        fontSize: 32,
+        fontSize: 21,
         marginBottom: 14,
-        marginTop: 10,
+        // marginTop: 10,
     }
 }));
 const MyText = styled(Typography)(({ theme }) => ({
+    letterSpacing: "0.01em",
     fontWeight: 400,
     fontSize: 33,
     lineHeight: "56px",
@@ -30,6 +40,29 @@ const MyText = styled(Typography)(({ theme }) => ({
         lineHeight: "16px",
     }
 }));
+const CustomButton = styled(Button)(({ theme }) => ({
+    padding: "21px 70px",
+    marginBottom: "26px",
+    fontWeight: 500,
+    fontSize: 24,
+    lineHeight: "28px",
+    textAlign: "center",
+    color: "#fff",
+    backgroundColor: "#1B1641",
+    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.08)",
+    borderRadius: 119,
+    '&:disabled': {
+        color: "#b0b0b0",
+    },
+    '&:hover': {
+        backgroundColor: "#1B1641",
+    },
+    [theme.breakpoints.down("sm")]:{
+        padding: "12px 30px",
+        fontSize: 12,
+        lineHeight: "20px",
+    }
+}));
 
 function RequestComponent() {
     const navigate = useNavigate();
@@ -40,11 +73,11 @@ function RequestComponent() {
         navigate("/")
     }
     return(
-        <Box>
-            <Box className="container" sx={{ display: "flex", flexDirection: "column", alignItems: "center", maxWidth: 650 }}>
-                <Box sx = {{mb: 92}}>
-                    <Box sx = {{mb: 22}}>
-                        <MyTextTitle>
+        <CustomBox>
+            <Box className="container" sx={{ display: "flex", flexDirection: "column", alignItems: "center", maxHeight: "100%"}}>
+                <Box sx = {{mb: 9.2, display: "flex", flexDirection: "column", alignItems: "center", maxWidth: 650, textAlign: "center"}}>
+                    <Box >
+                        <MyTextTitle sx = {{mb: 2.2}}>
                             ЗАЯВКА ОТПРАВЛЕНА!
                         </MyTextTitle>
                     </Box>
@@ -54,17 +87,17 @@ function RequestComponent() {
                         </MyText>
                     </Box>
                 </Box>
-                <Box>
-                    <Box sx = {{mb: 30}}>
+                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", maxWidth: 650, textAlign: "center", }}>
+                    <Box sx = {{mb: 3,}}>
                         <MyText>
                             Мы свяжемся с Вами в течении 24 часов после проверки ваших данных
                         </MyText>
                     </Box>
-                    <Button onClick={goToMainPage} type="submit">НА ГЛАВНУЮ</Button>
+                    <CustomButton onClick={goToMainPage} type="submit" >НА ГЛАВНУЮ</CustomButton>
                 </Box>
                 
             </Box>
-        </Box>
+        </CustomBox>
     )
 }
 
