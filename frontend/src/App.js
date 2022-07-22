dd4632f8784799892afe07a107769186bf50bcef
Okonesh02 <freegod2805@gmail.com>
2022-07-22 11:39:28 +0900

    fixed header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,22 +22,22 @@ import {
 import Topic from './components/topic/topic';
 function App() {
   return (
-    <div className="App" style={{ display: 'flex', flexDirection: 'column', height: "100vh" }}>
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', height: '100%', }}>
       <BrowserRouter>
         <Header/>
-          <div style={{ flexGrow: 1 }}>
-            <Routes>
-              <Route path="" element={<Home/>}></Route>
-              <Route path="/team" element={<Team/>}></Route>
-              <Route path="/contacts" element={<Contacts/>}></Route>
-              <Route path="/interactions" element={<Interactions/>}></Route>
-              <Route path="/application" element={<Application/>}></Route>
-              <Route path="/request/:id" element={<Request/>}></Route>
-              <Route path="/faq" element={<Faq/>}></Route>
-              <Route path="/faq/topic/:id" element={<Topic/>}></Route>
-              <Route path="/faq/page/:id" element={<Registration/>}></Route>
-            </Routes>
-          </div>
+        <div style={{ flexGrow: 1, height: '100%' }}>
+          <Routes>
+            <Route path="" element={<Home/>}></Route>
+            <Route path="/team" element={<Team/>}></Route>
+            <Route path="/contacts" element={<Contacts/>}></Route>
+            <Route path="/interactions" element={<Interactions/>}></Route>
+            <Route path="/application" element={<Application/>}></Route>
+            <Route path="/request/:id" element={<Request/>}></Route>
+            <Route path="/faq" element={<Faq/>}></Route>
+            <Route path="/faq/topic/:id" element={<Topic/>}></Route>
+            <Route path="/faq/page/:id" element={<Registration/>}></Route>
+          </Routes>
+        </div>
         <Footer style={{ justifySelf: "end" }} />
       </BrowserRouter>
     </div>
